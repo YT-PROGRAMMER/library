124335850b47b3c691db43f249807a291db2b5b6
Youssef <ytelmalwany@gmail.com>
2025-03-30 01:56:46 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+let inp_name = document.querySelector("#name")
+let inp_password = document.querySelector("#password")
+let check = document.querySelector("[type = 'checkbox']")
+let submit = document.querySelector(`[type = "submit"]`)
+
+submit.addEventListener("click" ,(e) => {
+    if(inp_name.value !== "" && inp_password.value !== "" && check.checked) {
+        e.preventDefault()
+        location.href = "login.html";
+        localStorage.setItem("name", inp_name.value);
+        localStorage.setItem("password", inp_password.value);
+    }
+})
+
+if (localStorage.name !== null && localStorage.password !== null) {
+    location.href = "login.html"
+}
+
+
+
+
+
